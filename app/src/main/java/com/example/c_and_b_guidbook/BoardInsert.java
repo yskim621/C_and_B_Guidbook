05889215cab018de1a672d5dd4b0aaf0ce6f09d7
yskim621 <yskim62100@gmail.com>
2020-09-24 22:11:57 +0900

    2020.09.16 Making game/hwatu and boardgame

---
+++ app/src/main/java/com/example/c_and_b_guidbook/BoardInsert.java
@@ -0,0 +1,14 @@
+package com.example.c_and_b_guidbook;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BoardInsert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board_insert);
+    }
+}
--- app/src/main/java/com/example/c_and_b_guidbook/MainActivity.java
+++ app/src/main/java/com/example/c_and_b_guidbook/MainActivity.java
@@ -17,9 +17,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    ListView trumpcardlistview;
+    ListView trumpcardlistview, hwatulistview, boardgamelistview;
     //출력할 데이터
-    ArrayList<Map<String, Object>> data;
+    ArrayList<Map<String, Object>> trumpcard, hwatu, boardgame;
     ArrayAdapter<String> adapter;
     Intent intent;
     @Override
@@ -28,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         trumpcardlistview = (ListView)findViewById(R.id.trumpcardlistview);
-        data = new ArrayList<>();
+        hwatulistview = (ListView)findViewById(R.id.hwatulistview);
+        boardgamelistview = (ListView)findViewById(R.id.boardgamelistview);
 
         //TabHost 찾아오기
         TabHost host = (TabHost)findViewById(R.id.host);
@@ -36,58 +37,116 @@ public class MainActivity extends AppCompatActivity {
         //탭 설정 시작
         host.setup();
 
-        //첫번째 탭 설정
-        TabHost.TabSpec spec = host.newTabSpec("탭1");
+        //Trumpcard 탭 설정
+        TabHost.TabSpec spec = host.newTabSpec("trumpcard");
         spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.trumpcard, null));
         spec.setContent(R.id.trumpcardlistview);
         host.addTab(spec);
+
+        trumpcard = new ArrayList<>();
 
         Map<String, Object> map = new HashMap<>();
         map.put("image", R.drawable.poker);
         map.put("title", "Poker");
         map.put("content", "5개 패의 족보가 가장 높은 사람이 승리하는 게임");
-        data.add(map);
+        trumpcard.add(map);
 
         map = new HashMap<>();
         map.put("image", R.drawable.black_jack);
         map.put("title", "Black_jack");
         map.put("content", "21에 가장 근접한 자가 이기는 게임");
-        data.add(map);
+        trumpcard.add(map);
 
         map = new HashMap<>();
         map.put("image", R.drawable.bacara);
         map.put("title", "Bacara");
         map.put("content", "2팀으로 나누어 베팅하며 낮은 수를 가진 팀이 승리");
-        data.add(map);
+        trumpcard.add(map);
 
         map = new HashMap<>();
         map.put("image", R.drawable.hoola);
         map.put("title", "Hoola");
         map.put("content", "모든 카드를 다 드랍하면 이기는 게임");
-        data.add(map);
+        trumpcard.add(map);
 
         //어댑터 생성
-        CardAdapter adapter = new CardAdapter(this, data);
+        CardAdapter adapter = new CardAdapter(this, trumpcard);
 
 
         trumpcardlistview.setAdapter(adapter);
 
-        //새로운 탭 생성
-        spec = host.newTabSpec("탭2");
+
+
+        //Hwatu 탭 생성
+        spec = host.newTabSpec("hwatu");
         //탭 아이콘 설정
         spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.hwatu, null));
         //탭에 보여질 내용 설정
-        spec.setContent(R.id.tab2);
+        spec.setContent(R.id.hwatulistview);
         //탭 추가
         host.addTab(spec);
 
-        spec = host.newTabSpec("탭3");
+        hwatu = new ArrayList<>();
+
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("image", R.drawable.gostop);
+        map1.put("title", "Gostop");
+        map1.put("content", "광/그림/피 등의 점수를 계산에서 2인 7점, 3인 3점을 먼저 내는 사람이 승리");
+        hwatu.add(map1);
+
+        map1 = new HashMap<>();
+        map1.put("image", R.drawable.seosda);
+        map1.put("title", "Seosda");
+        map1.put("content", "2장 혹은 3장의 패로 족보에 의거하여 베팅");
+        hwatu.add(map1);
+
+        //어댑터 생성
+        adapter = new CardAdapter(this, hwatu);
+
+        hwatulistview.setAdapter(adapter);
+
+
+
+
+        spec = host.newTabSpec("boardgame");
         //탭 아이콘 설정
         spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.boardgame, null));
         //탭에 보여질 내용 설정
-        spec.setContent(R.id.tab3);
+        spec.setContent(R.id.boardgamelistview);
         //탭 추가
         host.addTab(spec);
+
+        boardgame = new ArrayList<>();
+
+        Map<String, Object> map2 = new HashMap<>();
+        map2.put("image", R.drawable.chess);
+        map2.put("title", "Chess");
+        map2.put("content", "체스는 가로와 세로가 각각 8줄씩 64칸으로 격자로 배열 된 체스보드에서 두 명의 플레이어가 피스들을 규칙에 따라 움직여 싸우는 보드 게임");
+        boardgame.add(map2);
+
+        map2 = new HashMap<>();
+        map2.put("image", R.drawable.go);
+        map2.put("title", "Go");
+        map2.put("content", "바둑 혹은 오로, 혁기, 는 두 사람이 흑과 백의 돌을 사각의 판 위에 번갈아 놓으며 집을 차지하는 것을 겨루는 놀이");
+        boardgame.add(map2);
+
+        map2 = new HashMap<>();
+        map2.put("image", R.drawable.janggi);
+        map2.put("title", "Janggi");
+        map2.put("content", "장기는 청과 홍 두 편으로 나뉘어 각 16개의 기물을 가지고 군대를 지휘하는 총사령관의 입장에서 작전을 구상, 수행하여 상대편의 왕을 잡는 추상 전략 보드 게임");
+        boardgame.add(map2);
+
+        map2 = new HashMap<>();
+        map2.put("image", R.drawable.omok);
+        map2.put("title", "Omok");
+        map2.put("content", "오목은 바둑판에 두 사람이 번갈아 돌을 놓아 가로나 세로, 대각선으로 다섯 개의 연속된 돌을 놓으면 이기는 놀이");
+        boardgame.add(map2);
+
+        //어댑터 생성
+        adapter = new CardAdapter(this, boardgame);
+
+        boardgamelistview.setAdapter(adapter);
+
     }
 
     @Override
